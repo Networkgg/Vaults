43dc13ad9c66b655ef767991a0c3387ed4646d7b
GTNick <nickpp1111@yahoo.com>
2016-07-20 18:36:38 -0400

    Update MainClass.php

--- scr/PV/MainClass.php
+++ scr/PV/MainClass.php
@@ -5,3 +5,12 @@ use  pocketmine\server
 use  pocketmine\plugin\PluginBase
 
 class: Vaults/main
+
+    public function onEnable(){
+        $this->getLogger()->info("Vaults is now Enabled!");
+    }
+
+    public function onDisable(){
+        $this->getLogger()->info("Vaults is now Disabled!");
+    }
+}
