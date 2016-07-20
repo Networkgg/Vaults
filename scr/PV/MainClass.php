c7ba23d8512d9465f58b69a071f3354e3261b18f
GTNick <nickpp1111@yahoo.com>
2016-07-20 18:53:53 -0400

    Update MainClass.php

--- scr/PV/MainClass.php
+++ scr/PV/MainClass.php
@@ -9,6 +9,14 @@ class: Vaults/main
     public function onEnable(){
         $this->getLogger()->info("Vaults is now Enabled!");
     }
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+    if(strtolower($command->getName()) === "pv"){
+        // Execute logic
+        return true;
+    }
+
+    return false;
+}
 
     public function onDisable(){
         $this->getLogger()->info("Vaults is now Disabled!");
